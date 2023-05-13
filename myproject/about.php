fa718a8a39fa2a8b3edfb973a435bd25334f2a81
Sunny-Nikam <124758675+Sunny-Nikam@users.noreply.github.com>
2023-05-13 14:26:32 +0530

    Add files via upload

---
+++ myproject/about.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title></title>
+    <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
+  <link rel="stylesheet" type="text/css" href="css/style.css">
+  <link href='https://fonts.googleapis.com/css?family=Josefin+Sans&display=swap' rel='stylesheet'>
+</head>
+<body>
+
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <a class="navbar-brand" href="#">Grand Tourism</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav ml-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
+      </li>
+      
+      <li class="nav-item">
+        <a class="nav-link" href="about.php">About</a>
+      </li>
+      
+      
+      
+      
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    </form>
+  </div>
+</nav>
+<div class="jumbotron">
+  <h1>Grand Tourism</h1>
+  <p>Welcome to our tourism website, where you can discover all the amazing destinations and experiences that await you. Whether you're a seasoned traveler or planning your very first trip, we are here to help you make the most of your journey.</p>
+</div> 
+<section class="my-5">
+    <div class="py-5">
+        <h2 class="text-center">About Us</h2>
+    </div>
+    <div class="container-fluid">
+    <div class="row">
+        <div class="col-lg-6 col-md-6 col-12">
+            <img src="images/f.jpg" class="img-fluid about.img">
+        </div>
+
+            <div class="col-lg-6 col-md-6 col-12">
+                <h2>Grand Tourism</h2>
+                <p> At our tourism , we are passionate about travel and dedicated to helping people experience the world's most incredible destinations. We believe that travel is not just a luxury, but a transformative experience that can broaden our horizons, connect us with other cultures, and inspire us to become better global citizens.</p>
+                <a href="about.php" class="btn btn-success"> Check More </a>
+    </div>
+</section>
+<footer>
+    <p class="p-3 bg-dark text-white text-center">@Sunny_Nikam</p>
+</footer>
+
+
+</body>
+</html>
---
+++ myproject/index.php
@@ -0,0 +1,197 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title></title>
+    <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
+  <link rel="stylesheet" type="text/css" href="css/style.css">
+  <link href='https://fonts.googleapis.com/css?family=Josefin+Sans&display=swap' rel='stylesheet'>
+</head>
+<body>
+
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <a class="navbar-brand" href="#">Grand Tourism</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav ml-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="index.php">Home<span class="sr-only">(current)</span></a>
+      </li>
+      
+      <li class="nav-item">
+        <a class="nav-link" href="about.php">About</a>
+      </li>
+
+      
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    </form>
+  </div>
+</nav>
+
+<div id="demo" class="carousel slide" data-ride="carousel">
+  <ul class="carousel-indicators">
+    <li data-target="#demo" data-slide-to="0" class="active"></li>
+    <li data-target="#demo" data-slide-to="1"></li>
+    <li data-target="#demo" data-slide-to="2"></li>
+  </ul>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="images/16.jpg" alt="PAris" width="1100" height="500">
+      <div class="carousel-caption">
+        <h3>Paris</h3>
+        <p>Paris one of the finest places to visit!</p>
+      </div>   
+    </div>
+    <div class="carousel-item">
+      <img src="images/21.jpg" alt="mo" width="1100" height="500">
+      <div class="carousel-caption">
+        <h3>Spain</h3>
+        <p>Spain is so much fun !</p>
+      </div>   
+    </div>
+    <div class="carousel-item">
+      <img src="images/xyz.jpg" alt="New York" width="1100" height="500">
+      <div class="carousel-caption">
+        <h3>Africa</h3>
+        <p>A place you should definetly visit !</p>
+      </div>   
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#demo" data-slide="prev">
+    <span class="carousel-control-prev-icon"></span>
+  </a>
+  <a class="carousel-control-next" href="#demo" data-slide="next">
+    <span class="carousel-control-next-icon"></span>
+  </a>
+</div>
+
+<section class="my-5">
+    <div class="py-5">
+        <h2 class="text-center">About Us</h2>
+    </div>
+    <div class="container-fluid">
+    <div class="row">
+        <div class="col-lg-6 col-md-6 col-12">
+            <img src="images/19.jpg" class="img-fluid about.img">
+        </div>
+
+            <div class="col-lg-6 col-md-6 col-12">
+                <h2>Hawaii</h2>
+                <p> Hawaii is known for its stunning beaches with crystal clear waters and soft white sand. Whether you want to surf, snorkel, or just relax in the sun, there's a beach for you.</p>
+                <a href="about.php" class="btn btn-success"> Check More </a>
+    </div>
+</section>
+
+<section class="my-5">
+    <div class="py-5">
+        <h2 class="text-center">Some More Places To Visit</h2>
+    </div>
+
+    <div class="container-fluid">
+        <div class="row"> 
+            <div class="col-lg-4 col-md-4 col-12">
+                 <div class="card" >
+           <img class="card-img-top" src="images/18.jpg" alt="Card image" >
+           <div class="card-body">
+            <h4 class="card-title">Australia</h4>
+              <p class="card-text"> Africa is home to some of the world's most incredible and unique wildlife, including lions, elephants, giraffes, and many more. You can go on a safari and see these amazing animals up close in their natural habitats.</p>
+                <a href="#" class="btn btn-primary stretched-link">For More Info</a>
+    </div>
+  </div>
+</div>
+<div class="col-lg-4 col-md-4 col-12">
+                 <div class="card" >
+           <img class="card-img-top" src="images/15.pg.jpg" alt="Card image" >
+           <div class="card-body">
+            <h4 class="card-title">Thailand</h4>
+              <p class="card-text">A place to definetly visit</p>
+                <a href="#" class="btn btn-primary stretched-link">For More Info</a>
+    </div>
+  </div>
+</div>
+<div class="col-lg-4 col-md-4 col-12">
+                 <div class="card" >
+           <img class="card-img-top" src="images/13.jpg" alt="Card image" >
+           <div class="card-body">
+            <h4 class="card-title">Iceland</h4>
+              <p class="card-text">Finest place to visit if u like snow</p>
+                <a href="#" class="btn btn-primary stretched-link">For More Info</a>
+    </div>
+  </div>
+</div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="my-5">
+    <div class="py-5">
+        <h2 class="text-center">Gallery</h2>
+    </div>
+
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-lg-4 col-md-4 col-12">
+                <img src="images/a.jpg" class="img-fluid pb-3">
+            </div>
+            <div class="col-lg-4 col-md-4 col-12">
+                <img src="images/b.jpg" class="img-fluid pb-3">
+            </div>
+            <div class="col-lg-4 col-md-4 col-12">
+                <img src="images/c.jpg" class="img-fluid pb-3">
+            </div>
+            <div class="col-lg-4 col-md-4 col-12">
+                <img src="images/d.jpg" class="img-fluid pb-3">
+            </div>
+            <div class="col-lg-4 col-md-4 col-12">
+                <img src="images/e.jpg" class="img-fluid pb-3">
+            </div>
+            <div class="col-lg-4 col-md-4 col-12">
+                <img src="images/f.jpg" class="img-fluid pb-3">
+            </div>
+        </div>
+    </div>
+    
+    <div class="w-50 m-auto">
+        <form action="userinfo.php" method="post">
+            <div class="form-group">
+                <label>Username</label>
+                <input type="text" name="user" autocomplete="off" class="form-control">
+            </div>
+            <div class="form-group">
+                <label>Email id</label>
+                <input type="text" name="email" autocomplete="off" class="form-control">
+            </div>
+            <div class="form-group">
+                <label>Mobile</label>
+                <input type="text" name="mobile" autocomplete="off" class="form-control">
+            </div>
+            <div class="form-group">
+                <label>Comments</label>
+                <textarea class="form-control" name="comments"></textarea>
+            </div>
+            <button type="submit" class="btn btn-success">Submit</button>
+        </form>
+    </div>
+
+
+</section>
+
+<footer>
+    <p class="p-3 bg-dark text-white text-center">@Sunny_Nikam</p>
+</footer>
+
+
+
+<script src="https://cdn.jsdelivr.net/npm/jquery@3.6.4/dist/jquery.slim.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
